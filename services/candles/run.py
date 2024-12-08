bd19310f995bf4b9cd0a4192dca84a3ab8fbbb1d
gil612 <sgba000@gmail.com>
2024-12-08 22:07:44 +0100

    Add run,, config and makerun to Candles

--- services/candles/run.py
+++ services/candles/run.py
@@ -1,4 +1,46 @@
+from datetime import timedelta
+from typing import Any, List, Optional, Tuple
+
 from loguru import logger
+from quixstreams.models import TimestampType
+
+
+def custom_ts_extractor(
+    value: Any,
+    headers: Optional[List[Tuple[str, bytes]]],
+    timestamp: float,
+    timestamp_type: TimestampType,
+) -> int:
+    """
+    Specifying a custom timestamp extractor to use the timestamp from the message payload
+    instead of Kafka timestamp.
+    """
+    # breakpoint
+    return value['timestamp_ms']
+
+
+def init_candle(trade: dict) -> dict:
+    """
+    Initialize a candle with the first trade
+    """
+    return {
+        'open': trade['price'],
+        'high': trade['price'],
+        'low': trade['price'],
+        'close': trade['price'],
+        'volume': trade['volume'],
+    }
+
+
+def update_candle(candle: dict, trade: dict) -> dict:
+    """
+    Update the candle with the latest trade
+    """
+    candle['close'] = trade['price']
+    candle['high'] = max(candle['high'], trade['price'])
+    candle['low'] = min(candle['low'], trade['price'])
+    candle['volume'] += trade['volume']
+    return candle
 
 
 def main(
@@ -28,20 +70,49 @@ def main(
 
     # Initialize the Quix application
     app = Application(
-        kafka_broker_address=kafka_broker_address,
-        kafka_consumer_group=kafka_consumer_group,
+        broker_address=kafka_broker_address,
+        consumer_group=kafka_consumer_group,
     )
 
     # Define the input and output topic
-    input_topic = app.get_topic(name=kafka_input_topic, value_deserializer='json')
-
-    output_topic = app.get_topic(
-        name=kafka_output_topic,
-        value_serializer='json',
+    input_topic = app.topic(
+        name=kafka_input_topic,
+        value_deserializer='json',
+        timestamp_extractor=custom_ts_extractor,
     )
-    # Create a streaming DataFrame
+
+    output_topic = app.topic(name=kafka_output_topic, value_serializer='json')
+    # Create a streaming DataFrame from the input topic
     sdf = app.dataframe(topic=input_topic)
+
+    # Define tumbling window
+    sdf = sdf.tumbling_window(timedelta(seconds=candle_seconds))
+
+    # Apply the redicer ti update the candle, or initalize it with the first trade
+    sdf = sdf.reduce(reducer=update_candle, initializer=init_candle)
+
+    # Emit all intermediate candles to make rhe system more responsive
+    sdf = sdf.current()
+    # If you wanted to emit the final candle only, you could do this:
+    # sdf = sdf.final()
+
+    # Note: you can pipe to emit the final candle only, you could do this example:
+    # https://quix.io/docs/quix-streams/windowing.html#updating-window-definitions
+
+    # push the candle to the output topic
+    sdf = sdf.to_topic(topic=output_topic)
+
+    # Start the application
+    app.run()
 
 
 if __name__ == '__main__':
-    main()
+    from config import config
+
+    main(
+        kafka_broker_address=config.kafka_broker_address,
+        kafka_input_topic=config.kafka_input_topic,
+        kafka_output_topic=config.kafka_output_topic,
+        kafka_consumer_group=config.kafka_consumer_group,
+        candle_seconds=config.candle_seconds,
+    )
