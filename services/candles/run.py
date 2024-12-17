b1b88e8d359be1594ee279305c0d897e69f3764d
gil612 <sgba000@gmail.com>
2024-12-17 19:42:28 +0100

    backfill tehnical indicators for all 4 services

--- services/candles/run.py
+++ services/candles/run.py
@@ -1,5 +1,5 @@
 from datetime import timedelta
-from typing import Any, List, Optional, Tuple
+from typing import Any, List, Literal, Optional, Tuple
 
 from loguru import logger
 from quixstreams import Application
@@ -57,6 +57,7 @@ def main(
     kafka_consumer_group: str,
     candle_seconds: int,
     emit_incomplete_candles: bool,
+    data_source: Literal['live', 'historical', 'test'],
 ):
     """
     3 steps:
@@ -71,6 +72,7 @@ def main(
         kafka_consumer_group (str): Kafka consumer group
         candle_seconds (int): Candle seconds
         emit_incomplete_candles (bool): Emit incomplete candles or just the final one
+        data_source (Literal['live', 'historical', 'test']): Data source
     Returns:
         None
     """
@@ -80,6 +82,7 @@ def main(
     app = Application(
         broker_address=kafka_broker_address,
         consumer_group=kafka_consumer_group,
+        auto_offset_reset='latest' if data_source == 'live' else 'earliest',
     )
 
     # Define the input and output topics
@@ -162,4 +165,5 @@ if __name__ == '__main__':
         kafka_consumer_group=config.kafka_consumer_group,
         candle_seconds=config.candle_seconds,
         emit_incomplete_candles=config.emit_incomplete_candles,
+        data_source=config.data_source,
     )
