f111ad6373508704a108f77739dbd44aba7c70a3
gil612 <sgba000@gmail.com>
2025-01-18 17:08:42 +0100

    Add Preproceesing to Feature_reader class

--- services/price-predictor/config.py
+++ services/price-predictor/config.py
@@ -5,14 +5,25 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class TrainingConfig(BaseSettings):
     model_config = SettingsConfigDict(env_file="training.settings.env")
 
-    feature_view_name: str = Field(alias="FEATURE_VIEW_NAME")
-    feature_view_version: int = Field(alias="FEATURE_VIEW_VERSION")
-    pair_to_predict: str = Field(alias="PAIR_TO_PREDICT")
-    candle_seconds: int = Field(alias="CANDLE_SECONDS")
-    prediction_seconds: int = Field(alias="PREDICTION_SECONDS")
-    pairs_as_features: list[str] = Field(alias="PAIRS_AS_FEATURES")
-    days_back: int = Field(alias="DAYS_BACK")
-    llm_model_name_news_signals: str = Field(alias="LLM_MODEL_NAME_NEWS_SIGNALS")
+    feature_view_name: str = Field(description="The name of the feature view")
+    feature_view_version: int = Field(description="The version of the feature view")
+    pair_to_predict: str = Field(description="The pair to train the model on")
+    candle_seconds: int = Field(description="The number of seconds per candle")
+    prediction_seconds: int = Field(
+        description="The number of seconds into the future to predict"
+    )
+    pairs_as_features: list[str] = Field(
+        description="The pairs to use for the features"
+    )
+    technical_indicators_as_features: list[str] = Field(
+        description="The technical indicators to use for from the technical_indicators feature group"
+    )
+    days_back: int = Field(
+        description="The number of days to consider for the historical data"
+    )
+    llm_model_name_news_signals: str = Field(
+        description="The name of the LLM model to use for the news signals"
+    )
 
 
 training_config = TrainingConfig()
